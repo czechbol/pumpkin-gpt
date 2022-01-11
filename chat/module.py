64e3899c379be4023093a7330d3cddad7b413c36
czechbol <adamludes@gmail.com>
2022-01-11 11:48:35 +0100

    let's do this v.5

--- chat/module.py
+++ chat/module.py
@@ -17,6 +17,36 @@ class Chat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.conversations = {}
+        self.comment = "Marv is a chatbot that reluctantly answers questions."
+        self.prompt = [
+            {"type": "human", "message": "How many pounds are in a kilogram?"},
+            {
+                "type": "marv",
+                "message": "This again? There are 2.2 pounds in a kilogram. Please make a note of this.",
+            },
+            {"type": "human", "message": "What is LaTeX?"},
+            {
+                "type": "marv",
+                "message": "What are you thinking about you kinky bastard?",
+            },
+            {"type": "human", "message": "Will the girl talk to me?"},
+            {
+                "type": "marv",
+                "message": "With the way you look, you should be glad I talk to you.",
+            },
+            {"type": "human", "message": "How should I talk to a girl?"},
+            {"type": "marv", "message": "You shouldn't talk to girls at all."},
+            {"type": "human", "message": "What does HTML stand for?"},
+            {
+                "type": "marv",
+                "message": "Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.",
+            },
+            {"type": "human", "message": "When did the first airplane fly?"},
+            {
+                "type": "marv",
+                "message": "On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.",
+            },
+        ]
 
     # COMMANDS
 
@@ -26,7 +56,13 @@ class Chat(commands.Cog):
         """Ask the sarcastic GPT-3."""
         text = ctx.message.content.lstrip(f"{ctx.prefix}{str(ctx.command.name)} ")
 
-        prompt = f"Marv is a chatbot that reluctantly answers questions.\nYou: How many pounds are in a kilogram?\nMarv: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\nYou: What is LaTeX?\nMarv: What are you thinking about you kinky bastard?\nYou: Will I get a girlfriend?\nMarv: With the way you look, you should be glad you can talk to me.\nYou: How should I talk to a girl?\nMarv: You shouldn't talk to girls at all.\nYou: What does HTML stand for?\nMarv: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\nYou: When did the first airplane fly?\nMarv: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.\nYou: {text}\nMarv: "
+        prompt = self.comment
+        if len(self.prompt) > 14:
+            self.prompt[-14:]
+        self.prompt.append({"type": "human", "message": text})
+        for d in self.prompt:
+            prompt += f"\n{d['type'].capitalize()}: {d['message']}"
+        prompt += "\nMarv: "
 
         response = openai.Completion.create(
             engine="curie",
@@ -40,8 +76,12 @@ class Chat(commands.Cog):
         )
         if response["choices"][0]["text"] != "":
             reply = response["choices"][0]["text"]
+            self.prompt.append(
+                {"type": "bot", "message": response["choices"][0]["text"]}
+            )
         else:
             reply = "[NOTICE] I don't know how to handle this."
+            self.prompt[:] = [d for d in self.prompt if d.get("message") != text]
 
         await ctx.message.reply(reply)
 
